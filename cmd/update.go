7693361d4a733fb9fbb7256ff9d0908d689e8096
Maor Bril <maor.bril@character.ai>
2026-01-03 08:57:18 -0800

    bump version to 0.3.0

--- cmd/update.go
+++ cmd/update.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	// Version is the current version of clauder
-	Version = "0.2.0"
+	Version = "0.3.0"
 
 	// GitHubOwner is the GitHub repository owner
 	GitHubOwner = "MaorBril"
